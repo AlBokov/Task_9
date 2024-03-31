27c397406f1169d2fe2fa5ef2665ee0848acf0f3
Alexander Bokov <alexander.bokov30@yandex.ru>
2024-03-31 16:57:54 +0300

    Добавление функции и тестов к ней, проверка 100% покрытия кода

--- src/main/java/ru/netology/statistic/StatisticsService.java
+++ src/main/java/ru/netology/statistic/StatisticsService.java
@@ -2,7 +2,7 @@ package ru.netology.statistic;
 
 public class StatisticsService {
     public long findMax(long[] incomes) {
-        long currentMax = incomes[0];
+        long currentMax = incomes[0]; // Максимальный доход
         for (long income : incomes) {
             if (currentMax < income) {
                 currentMax = income;
